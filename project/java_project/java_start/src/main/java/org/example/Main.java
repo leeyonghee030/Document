90ea09e88e3659168fa4a1f44d9421e02c67fefa
leeyonghee030 <leeyonghee030@gmail.com>
2026-03-14 01:03:27 +0900

    옵시디언 불피요한 파일  정리

--- project/java_project/java_start/src/main/java/org/example/Main.java
+++ project/java_project/java_start/src/main/java/org/example/Main.java
@@ -4,6 +4,12 @@ package org.example;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+
+        String a = "1";
+        int b = 1;
+
+        System.out.println(a+1+1);
+        System.out.println(b+1);
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
         System.out.printf("Hello and welcome!");
